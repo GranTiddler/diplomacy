cb365409ea7c941c403e8ce1bc913ba0a33e0e7e
GranTiddler <grantdriddle7@gmail.com>
2023-11-08 23:53:11 -0700

    coast support

--- project/html/scripts/javascript/get_moves.js
+++ project/html/scripts/javascript/get_moves.js
@@ -2,7 +2,8 @@ var moves = {};
 
 
 
-var units = { "Con": { "type": "A" }, "Ank": { "type": "F" }, "Smy": { "type": "F" }, "Kie": { "type": "A" }, "Mun": { "type": "A" } };
+var units = { "Con": { "type": "A" }, "Ank": { "type": "F" }, "Smy": { "type": "A" }, "Kie": { "type": "A" }, "Mun": { "type": "A" }, "Mos": { "type": "A" }, "Ukr": { "type": "A" }, "Sev": { "type": "F" }, "Stp": { "type": "F", "coast": "SC" } };
+var controlledUnits
 
 var showCoasts = true;
 var coastTerritories = [];
@@ -68,6 +69,7 @@ function territoryClick(id) {
         }
     }
     else if (units[id]) {
+        action = "m";
         window.isSelected = true;
         window.selected = id;
 
@@ -335,12 +337,19 @@ function createGraphics(json) {
     }
 }
 
-function drawMove(json, territory, target) {
+function drawMove(json, territory, target, c1, c2) {
 
-    console.log(target)
 
     var p1 = json["map"][territory]["locations"]["unit"]
     var p2 = json["map"][target]["locations"]["unit"]
+
+    if (c1) {
+        var p1 = json["map"][territory]["locations"]["coasts"][c1]["unit"]
+    }
+    if (c2) {
+        var p2 = json["map"][target]["locations"]["coasts"][c2]["unit"]
+
+    }
 
     var dif = [p2[0] - p1[0], p2[1] - p1[1]];
 
@@ -354,8 +363,8 @@ function drawMove(json, territory, target) {
     var normalized = [Math.sin(angle), Math.cos(angle)]
 
 
-    p1 = [p1[0] + (normalized[0] * 9), p1[1] + (normalized[1] * 9)]
-    p2 = [p2[0] - (normalized[0] * 9), p2[1] - (normalized[1] * 9)]
+    p1 = [p1[0] + (normalized[0] * 12), p1[1] + (normalized[1] * 12)]
+    p2 = [p2[0] - (normalized[0] * 12), p2[1] - (normalized[1] * 12)]
 
 
     el = document.createElementNS('http://www.w3.org/2000/svg', 'path')
@@ -371,8 +380,8 @@ function drawMove(json, territory, target) {
 
     var arrowAngle = Math.PI * .75
 
-    arrow1 = [Math.sin(angle-arrowAngle) * 10, Math.cos(angle-arrowAngle) * 10]
-    arrow2 = [Math.sin(angle+arrowAngle) * 10, Math.cos(angle+arrowAngle) * 10]
+    arrow1 = [Math.sin(angle - arrowAngle) * 10, Math.cos(angle - arrowAngle) * 10]
+    arrow2 = [Math.sin(angle + arrowAngle) * 10, Math.cos(angle + arrowAngle) * 10]
 
     el.setAttribute('d', "M" + (p2[0] + arrow1[0]) + "," + (p2[1] + arrow1[1]) + "L" + p2[0] + "," + p2[1] + "L" + (p2[0] + arrow2[0]) + "," + (p2[1] + arrow2[1]))
 
@@ -387,9 +396,13 @@ function drawMove(json, territory, target) {
 
 
 
-function drawHold(json, territory) {
+function drawHold(json, territory, coast = false) {
 
     var position = json["map"][territory]["locations"]["unit"]
+
+    if (coast) {
+        position = json["map"][territory]["locations"]["coasts"][coast]["unit"]
+    }
 
     el = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
     el.setAttribute('r', '6')
@@ -423,17 +436,38 @@ function drawConvoy(json, territory) {
     el.setAttribute('stroke-width', '2')
 
 
+
+
+
     var units = document.getElementById('orders');
     units.appendChild(el)
 
 }
 
-function drawSupport(json, territory, target, targettarget) {
+
+function lerp(a, b, c) {
+    return [a[0] * c + b[0] * (1 - c), a[1] * c + b[1] * (1 - c)]
+}
+
+function drawSupport(json, territory, target, targettarget, c1 = false, c2 = false, c3 = false) {
 
 
+    if (c1) {
+        p1 = json["map"][territory]["locations"]["coasts"][c1]["unit"]
+    }
     var p1 = json["map"][territory]["locations"]["unit"]
     var p2 = json["map"][target]["locations"]["unit"]
     var p3 = json["map"][targettarget]["locations"]["unit"]
+
+    if (c1) {
+        p1 = json["map"][territory]["locations"]["coasts"][c1]["unit"]
+    }
+    if (c2) {
+        p2 = json["map"][target]["locations"]["coasts"][c2]["unit"]
+    }
+    if (c3) {
+        p3 = json["map"][targettarget]["locations"]["coasts"][c3]["unit"]
+    }
 
     var dif = [p2[0] - p1[0], p2[1] - p1[1]];
     var angle = Math.atan2(dif[0], dif[1])
@@ -460,22 +494,37 @@ function drawSupport(json, territory, target, targettarget) {
 
     }
 
-    var wAvg = 0.5
+
+    var w1 = 0.5
+    var w2 = 0.5;
+    var w3 = 0.65;
+    var w4 = 0.8;
+
+    var a1 = lerp(p2, p3, w1)
+    var a2 = lerp(p1, a1, w2)
+    var a3 = lerp(p3, p2, w3)
+    var a4 = lerp(p3, p2, w4)
+
+
+
 
     el = document.createElementNS('http://www.w3.org/2000/svg', 'path')
     // el.setAttribute('d', "M" + p1[0] + "," + p1[1] + "Q" + p2[0] + "," + p2[1] + "," + p3[0] + "," + p3[1])
-    el.setAttribute('d', "M" + p1[0] + "," + p1[1] + "Q" + p2[0] + "," + p2[1] + "," + (p2[0] * wAvg + p3[0] * (1 - wAvg)) + "," + (p2[1] * wAvg + p3[1] * (1 - wAvg)) + "L" + p3[0] + "," + p3[1])
+    // el.setAttribute('d', "M" + p1[0] + "," + p1[1] + "Q" + p2[0] + "," + p2[1] + "," + (p2[0] * wAvg + p3[0] * (1 - wAvg)) + "," + (p2[1] * wAvg + p3[1] * (1 - wAvg)) + "L" + p3[0] + "," + p3[1])
+    // el.setAttribute('d', "M" + p1[0] + "," + p1[1] + "L" + (p1[0] + dif3[0] * 0.5) + "," + (p1[1] + dif3[1] * 0.5) + "," + p3[0] + "," + p3[1] + "," + p2[0] + "," + p2[1] + "," + "L" + p3[0] + "," + p3[1])
 
 
     if (target == targettarget) {
         el.setAttribute('d', "M" + p1[0] + "," + p1[1] + "L" + p3[0] + "," + p3[1] + "Z")
-
+    }
+    else {
+        el.setAttribute('d', "M" + p1[0] + "," + p1[1] + "C" + a2[0] + " " + a2[1] + " " + p2[0] + " " + p2[1] + " " + a3[0] + " " + a3[1] + "L" + a4[0] + " " + a4[1],)
     }
 
 
     el.setAttribute('stroke', '#000')
     el.setAttribute('fill-opacity', '0')
-    el.setAttribute('stroke-dasharray', '4 5')
+    el.setAttribute('stroke-dasharray', '2 2')
 
 
     el.setAttribute('stroke-width', '3')
@@ -483,39 +532,30 @@ function drawSupport(json, territory, target, targettarget) {
     var units = document.getElementById('orders');
     units.appendChild(el)
 
-    return;
-    var p1 = json["map"][territory]["locations"]["unit"]
-    var p2 = json["map"][target]["locations"]["unit"]
-    var p3 = json["map"][targettarget]["locations"]["unit"]
 
-    var dif23 = [p3[0] - p2[0], p3[1] - p2[1]];
-    var angle23 = Math.atan2(dif23[0], dif23[1])
-    var normalized23 = [Math.sin(angle23), Math.cos(angle23)]
-
-    p3 = [p3[0] - (normalized23 * 9), p3[1] - (normalized23 * 9)]
+    if (target != targettarget) {
 
 
+        el = document.createElementNS('http://www.w3.org/2000/svg', 'path')
 
-    var dif12 = [p2[0] - p1[0], p2[1] - p1[1]];
-    var angle12 = Math.atan2(dif12[0], dif12[1])
-    var normalized12 = [Math.sin(angle12), Math.cos(angle12)]
+        dif = [p3[0] - p2[0], p3[1] - p2[1]];
+        angle = Math.atan2(dif[0], dif[1]) + Math.PI / 2
+        normalized = [Math.sin(angle), Math.cos(angle)]
 
-    // p1 = [p1[0] + dif[0] * 0.1, p1[1] + dif[1] * 0.1]
-
-
-    el = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-
-    el.setAttribute('d', "M" + p1[0] + "," + p1[1] + "Q" + p2[0] + "," + p2[1] + "," + p3[0] + "," + p3[1])
-
-    el.setAttribute('stroke', '#000')
-    el.setAttribute('fill-opacity', '0')
-    el.setAttribute('stroke-dasharray', '4 5')
+        el.setAttribute('d', "M" + (a4[0] + (normalized[0]) * 10) + "," + (a4[1] + (normalized[1]) * 10) + "L" + (a4[0] - (normalized[0]) * 10) + "," + (a4[1] - (normalized[1]) * 10))
 
 
-    el.setAttribute('stroke-width', '3')
 
-    var units = document.getElementById('orders');
-    units.appendChild(el)
+        el.setAttribute('stroke', '#000')
+        el.setAttribute('fill-opacity', '0')
+
+
+        el.setAttribute('stroke-width', '3')
+
+        units.appendChild(el)
+
+    }
+
 }
 
 function drawUnit(json, territory, Type, country, coast, build = false) {
@@ -541,13 +581,22 @@ function drawUnit(json, territory, Type, country, coast, build = false) {
         el.setAttribute('d', "M" + (position[0] - 6) + "," + (position[1] - 2) + "L" + (position[0] + 6) + "," + (position[1] - 2) + "L" + position[0] + "," + (position[1] + 5) + "Z")
     }
 
+    if (build) {
+        el.setAttribute('fill-opacity', 0)
+        el.setAttribute('stroke-dasharray', '2 2')
+        el.setAttribute('stroke', "#000")
+        el.setAttribute('stroke-width', '1')
 
-    el.setAttribute('stroke', json["game"]["countries"][country]["color"])
-    el.setAttribute('fill', json["game"]["countries"][country]["color"])
-    el.setAttribute('fill-opacity', 0.6)
 
-    el.setAttribute('stroke-width', '2')
-    el.setAttribute("font-size", "8px")
+    }
+    else {
+        el.setAttribute('stroke', json["game"]["countries"][country]["color"])
+        el.setAttribute('fill', json["game"]["countries"][country]["color"])
+        el.setAttribute('fill-opacity', 0.6)
+        el.setAttribute('stroke-width', '2')
+    }
+
+
 
 
 
@@ -567,18 +616,50 @@ function drawOrders(json, moves) {
 
     for (var key in moves) {
 
+        var c1 = false
+        var c2 = false
+
+
+
+
         switch (moves[key]["action"]) {
             case "m":
-                drawMove(json, key, moves[key]["move"])
+                var m = moves[key]["move"].substring(0, 3)
+
+                if (moves[key]["move"].charAt(3)) {
+                    c1 = moves[key]["move"].substring(4)
+                }
+                drawMove(json, key, m, units[key]["coast"], c1)
                 break;
             case "h":
-                drawHold(json, key)
+                drawHold(json, key, units[key]["coast"])
                 break;
             case "c":
                 drawConvoy(json, key)
                 break;
             case "s":
-                drawSupport(json, key, moves[key]["move"][0], moves[key]["move"][1])
+
+                var m1 = moves[key]["move"][0].substring(0, 3)
+                var m2 = moves[key]["move"][1].substring(0, 3)
+                var c1;
+                var c2;
+
+
+                if (moves[key]["move"][0] == moves[key]["move"][1] && units[moves[key]["move"][1]]["coast"]) {
+                    c1 = units[moves[key]["move"][1]]["coast"];
+                    c2 = c1
+                }
+                else {
+                    if (moves[key]["move"][0].charAt(3)) {
+                        c1 = moves[key]["move"][0].substring(4)
+                    }
+
+                    if (moves[key]["move"][1].charAt(3)) {
+                        c2 = moves[key]["move"][1].substring(4)
+                    }
+                }
+
+                drawSupport(json, key, m1, m2, units[key]["coast"], c1, c2)
                 break;
         }
     }
@@ -592,6 +673,7 @@ document.addEventListener('keydown', (event) => {
         setAction(name);
     }
     else if (name == "Escape") {
+        isSelected = false;
         setAction("m");
     }
 }, false);
