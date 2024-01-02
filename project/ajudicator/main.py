fb382f94701ed352e411948df40ad77d0479fbb5
GranTiddler <grantdriddle7@gmail.com>
2024-01-01 19:11:41 -0700

    code skeleton

--- project/ajudicator/main.py
+++ project/ajudicator/main.py
@@ -9,6 +9,18 @@ def home():
     return "AAAAAAA"
 
 
+def schedule(boardNum):
+    return
+
+def adjudicate(boardNum):
+    return
+
+def getAdjudicateStatus(boardNum):
+    return
+
+def attemptAdjudiate(boardNum):
+    return
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
